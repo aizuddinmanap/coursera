fde06cca9b2f840b7af018c11c2900d5aa4e6a22
aizuddin manap <aizuddinmanap@gmail.com>
2017-02-27 15:26:15 +0800

    add facebook graph for json test

---
+++ facebook/profile.R
@@ -0,0 +1,9 @@
+library(jsonlite)
+
+token = "EAACEdEose0cBAGLwFUq6YbwqiAN1LrYuNd8o2kGzSYeFBIZBhawgQc6Tf7mFnIldgbPrVOqbZBVxLyFDdG38IH8mvza88krg4oxZBHsAHZBSROpbHO7OFXd2YskGv9LTug0bleHCeMtIXLJkcHj5nBPUBhdEPl3PFJitiVfgI76ZAVHIMo0SuCXbatOdN5ugZD"
+
+url <- paste0("https://graph.facebook.com/me?fields=id,name&access_token=", token)
+
+profile <- fromJSON(txt=url)
+
+View(profile)
