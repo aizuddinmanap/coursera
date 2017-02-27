283f1e8fd58d7c1bc20f3f11f98b254b4b863337
aizuddin manap <aizuddinmanap@gmail.com>
2017-02-27 11:29:04 +0800

    set folder

--- getting-data/getting_data.R
+++ getting-data/getting_data.R
@@ -1,5 +1,14 @@
-download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34",destfile="bptms.csv",method="libcurl")
+fileURL <- "http://data.stats.gov.my/download_bptms.php?cat=1&id=34"
+
+
+if (! dir.exists("files")) {
+  dir.create("files")
+}
+
+if (!file.exists(destfile)) {
+  download.file(fileURL , destfile, method="libcurl")
+}
 
 library(readr)
-bptms <- read_csv("./bptms.csv")
+bptms <- read_csv(destfile)
 View(bptms)
